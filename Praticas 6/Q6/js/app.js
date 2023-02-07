78c0c08ab424a6740479d7f1ced7ef3be45fd7d2
Gabriel Vasconcelos <97545157+gabriel-vasconcelos-ft@users.noreply.github.com>
2023-02-07 19:56:16 -0300

    Update app.js

--- Praticas 6/Q6/js/app.js
+++ Praticas 6/Q6/js/app.js
@@ -1,1 +1,21 @@
-let array1 = [10,11,12,14,15];
+function factorial(num) {
+  if (num === 0) {
+    return 1;
+  }
+  let result = [1];
+  for (let i = 1; i <= num; i++) {
+    let carry = 0;
+    for (let j = 0; j < result.length; j++) {
+      let product = result[j] * i + carry;
+      result[j] = product % 10;
+      carry = Math.floor(product / 10);
+    }
+    while (carry > 0) {
+      result.push(carry % 10);
+      carry = Math.floor(carry / 10);
+    }
+  }
+  console.log(result.reverse().join(''));
+}
+
+factorial(5);
